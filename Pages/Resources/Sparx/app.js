8e375fdf658b2ac96fd0f8a739a9df7c9093d4b1
Alex Lo Storto <lostorto.business@gmail.com>
2023-01-17 18:44:45 +0000

    Add Sparx code

---
+++ Pages/Resources/Sparx/app.js
@@ -0,0 +1,32 @@
+console.clear();
+credits();
+
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+// PROGRAM
+const copyButton = document.querySelector('.copy-button');
+
+async function getCode() {
+    response = await fetch('console.js');
+    text = response.text();
+
+    return text
+}
+
+copyButton.addEventListener('click', async () => {
+    navigator.clipboard.writeText(await getCode()).then(async function() {
+        console.log('Copying to clipboard was successful!');
+        copyButton.textContent = 'Copied!';
+        await sleep(1000);
+        copyButton.textContent = 'Copy';
+    }, async function(err) {
+        copyButton.textContent = 'Failed to copy';
+        await sleep(1000);
+        copyButton.textContent = 'Copy';
+        console.error('Could not copy text: ', err);
+    });
+})
