2510c2da1018f3ec0f6514132c315409ea52771c
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-09 21:56:01 +0000

    Add Cosine Rule Formula to Trignometry page

--- Pages/Resources/Trigonometry/app.js
+++ Pages/Resources/Trigonometry/app.js
@@ -14,13 +14,13 @@ function round(number, decimalPlaces) {
 }
 
 
-// PROGRAM
-const inputA = document.querySelector('#a');
-const inputB = document.querySelector('#b');
-const inputC = document.querySelector('#c');
-const inputAlpha = document.querySelector('#alpha');
-const inputBeta = document.querySelector('#beta');
-const output = document.querySelector('#output');
+// TRIGIONOMETRY
+const inputATrig = document.querySelector('#a-trig');
+const inputBTrig = document.querySelector('#b-trig');
+const inputCTrig = document.querySelector('#c-trig');
+const inputAlphaTrig = document.querySelector('#alpha-trig');
+const inputBetaTrig = document.querySelector('#beta-trig');
+const outputTrig = document.querySelector('#output-trig');
 
 let changedVariables = [];
 
@@ -40,17 +40,17 @@ async function trigonometry() {
         return radians * 180/Math.PI;
     }
 
-    const a = inputA.value;
-    const b = inputB.value;
-    const c = inputC.value;
-    const alpha = inputAlpha.value;
-    const beta = inputBeta.value;
+    const a = inputATrig.value;
+    const b = inputBTrig.value;
+    const c = inputCTrig.value;
+    const alpha = inputAlphaTrig.value;
+    const beta = inputBetaTrig.value;
 
-    let aValue = inputA.value;
-    let bValue = inputB.value;
-    let cValue = inputC.value;
-    let alphaValue = inputAlpha.value;
-    let betaValue = inputBeta.value;
+    let aValue = inputATrig.value;
+    let bValue = inputBTrig.value;
+    let cValue = inputCTrig.value;
+    let alphaValue = inputAlphaTrig.value;
+    let betaValue = inputBetaTrig.value;
 
     // input a 
     if (inputs.includes('a') && inputs.includes('b')) {
@@ -99,53 +99,138 @@ async function trigonometry() {
 
     // input alpha 
     else if (inputs.includes('alpha') && inputs.includes('beta')) {
-        output.innerHTML = '<span class="orange-text">Hint</span>: Try adding the length of a <span class="orange-text">side</span>.';
+        outputTrig.innerHTML = '<span class="orange-text">Hint</span>: Try adding the length of a <span class="orange-text">side</span>.';
         await sleep(2000);
-        output.innerHTML = '<span class="orange-text">Hint</span>: Angles are calculated in <span class="orange-text">degrees</span>.';
+        outputTrig.innerHTML = '<span class="orange-text">Hint</span>: Angles are calculated in <span class="orange-text">degrees</span>.';
     }
 
-    inputA.value = aValue;
-    inputB.value = bValue;
-    inputC.value = cValue;
-    inputAlpha.value = alphaValue;
-    inputBeta.value = betaValue;
+    inputATrig.value = aValue;
+    inputBTrig.value = bValue;
+    inputCTrig.value = cValue;
+    inputAlphaTrig.value = alphaValue;
+    inputBetaTrig.value = betaValue;
 
     if (isNaN(aValue) || isNaN(bValue) || isNaN(cValue) || isNaN(alphaValue) || isNaN(betaValue)) {
-        output.innerHTML = '<span class="orange-text">Error</span>: Angle α and Angle β must be <span class="orange-text">bigger</span> than <span class="orange-text">zero</span>.';
+        outputTrig.innerHTML = '<span class="orange-text">Error</span>: Angle α and Angle β must be <span class="orange-text">bigger</span> than <span class="orange-text">zero</span>.';
         await sleep(2000);
-        output.innerHTML = '<span class="orange-text">Hint</span>: Angles are calculated in <span class="orange-text">degrees</span>.';
+        outputTrig.innerHTML = '<span class="orange-text">Hint</span>: Angles are calculated in <span class="orange-text">degrees</span>.';
     }
 }
 
-inputA.oninput = function() {
+inputATrig.oninput = function() {
     if (changedVariables.length == 0 || changedVariables[changedVariables.length - 1] != 'a') {
         changedVariables.push('a');
     }
     trigonometry();
 }
 
-inputB.oninput = function() {
+inputBTrig.oninput = function() {
     if (changedVariables.length == 0 || changedVariables[changedVariables.length - 1] != 'b') {
         changedVariables.push('b');
     }
     trigonometry();
 }
 
-inputC.oninput = function() {
+inputCTrig.oninput = function() {
     if (changedVariables.length == 0 || changedVariables[changedVariables.length - 1] != 'c') {
         changedVariables.push('c');
     }
     trigonometry();
 }
 
-inputAlpha.oninput = function() {
+inputAlphaTrig.oninput = function() {
     if (changedVariables.length == 0 || changedVariables[changedVariables.length - 1] != 'alpha') {
         changedVariables.push('alpha');
     }    trigonometry();
 }
 
-inputBeta.oninput = function() {
+inputBetaTrig.oninput = function() {
     if (changedVariables.length == 0 || changedVariables[changedVariables.length - 1] != 'beta') {
         changedVariables.push('beta');
     }    trigonometry();
+}
+
+
+// COSINE RULE
+const inputACosine = document.querySelector('#a-cosine');
+const inputBCosine = document.querySelector('#b-cosine');
+const inputCCosine = document.querySelector('#c-cosine');
+const inputGammaCosine = document.querySelector('#gamma-cosine');
+const outputCosine = document.querySelector('#output-cosine');
+
+let changedVariablesCosine = [];
+
+async function cosine() {
+    if (changedVariablesCosine.length <= 1) { return }
+
+    const inputs = changedVariablesCosine.slice(changedVariablesCosine.length-3);
+    console.log(inputs.join(', '));
+
+    // Computation
+    function toRadians(degrees) {
+        return degrees * Math.PI/180;
+    }
+
+    function toDegrees(radians) {
+        return radians * 180/Math.PI;
+    }
+
+    const a = inputACosine.value;
+    const b = inputBCosine.value;
+    const c = inputCCosine.value;
+    const gamma = inputGammaCosine.value;
+
+    let aValue = inputACosine.value;
+    let bValue = inputBCosine.value;
+    let cValue = inputCCosine.value;
+    let gammaValue = inputGammaCosine.value;
+
+    if (inputs.includes('a') && inputs.includes('b') && inputs.includes('c')) {
+        gammaValue = round(toDegrees(Math.acos((a**2 + b**2 - c**2) / (2*a*b))), 3)
+    } else if (inputs.includes('a') && inputs.includes('b') && inputs.includes('gamma')) {
+        cValue = round((a**2 + b**2 - 2*a*b*Math.cos(toRadians(gamma)))**(1/2), 3)
+    } else if (inputs.includes('a') && inputs.includes('c') && inputs.includes('gamma')) {
+        bValue = round(a * Math.cos(toRadians(gamma)) + (c**2 - a**2 * (Math.sin(toRadians(gamma))**2))**(1/2), 3)
+    } else if (inputs.includes('b') && inputs.includes('c') && inputs.includes('gamma')) {
+        aValue = round(b * Math.cos(toRadians(gamma)) + (c**2 - b**2 * (Math.sin(toRadians(gamma))**2))**(1/2), 3)
+    } 
+
+    inputACosine.value = aValue;
+    inputBCosine.value = bValue;
+    inputCCosine.value = cValue;
+    inputGammaCosine.value = gammaValue;
+
+    if (isNaN(aValue) || isNaN(bValue) || isNaN(cValue) || isNaN(gammaValue)) {
+        outputCosine.innerHTML = '<span class="orange-text">Error</span>: You f*cked up.';
+        await sleep(2000);
+        outputCosine.innerHTML = '<span class="orange-text">Hint</span>: Angles are calculated in <span class="orange-text">degrees</span>.';
+    }
+}
+
+inputACosine.oninput = function() {
+    if (changedVariablesCosine.length == 0 || changedVariablesCosine[changedVariablesCosine.length - 1] != 'a') {
+        changedVariablesCosine.push('a');
+    }
+    cosine();
+}
+
+inputBCosine.oninput = function() {
+    if (changedVariablesCosine.length == 0 || changedVariablesCosine[changedVariablesCosine.length - 1] != 'b') {
+        changedVariablesCosine.push('b');
+    }
+    cosine();
+}
+
+inputCCosine.oninput = function() {
+    if (changedVariablesCosine.length == 0 || changedVariablesCosine[changedVariablesCosine.length - 1] != 'c') {
+        changedVariablesCosine.push('c');
+    }
+    cosine();
+}
+
+inputGammaCosine.oninput = function() {
+    if (changedVariablesCosine.length == 0 || changedVariablesCosine[changedVariablesCosine.length - 1] != 'gamma') {
+        changedVariablesCosine.push('gamma');
+    }    
+    cosine();
 }
