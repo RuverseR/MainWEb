f6a5d704a35fc142e8d4260d94d667f86b28291f
Alex Lo Storto <lostorto.business@gmail.com>
2023-03-19 11:44:01 +0000

    Reformat Navbar component

--- Assets/PHP/component.js
+++ Assets/PHP/component.js
@@ -16,77 +16,44 @@ class Navbar extends HTMLElement {
           </nav>
           <ul class="navbar-items">
             <li>
-              <a
-                class="navbar-item"
-                href="../../../Assets/Documents/CV 2 (PDF).pdf"
-                target="_blank"
-                >Resume</a
-              >
+              <a class="navbar-item" href="../../../Assets/Documents/CV 2 (PDF).pdf" target="_blank" >Resume</a>
             </li>
             <li class="resources-button">
               <a class="navbar-item" href="#">Resources</a>
               <div class="nav-resources">
                 <ul>
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Exam_Timetable/index.html"
-                    >Exam Timetable</a
-                  >
+                    <a href="../../../Pages/Resources/Exam_Timetable/index.html">Exam Timetable</a>
                   </li> 
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Trigonometry/index.html"
-                    >Trigonometry</a
-                  >
+                    <a href="../../../Pages/Resources/Trigonometry/index.html">Trigonometry</a>
                   </li>  
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Leaderboard/index.html"
-                    >Leaderboard</a
-                  >
+                    <a href="../../../Pages/Resources/Leaderboard/index.html">Leaderboard</a>
                   </li>  
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Insta_Size/index.html"
-                    >Insta Size</a
-                  >
+                    <a href="../../../Pages/Resources/Insta_Size/index.html">Insta Size</a>
                   </li>  
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Quadratics/index.html"
-                    >Quadratics</a
-                  >
+                    <a href="../../../Pages/Resources/Quadratics/index.html">Quadratics</a>
                   </li>
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Sequences/index.html"
-                    >Sequences</a
-                  >
+                    <a href="../../../Pages/Resources/Sequences/index.html">Sequences</a>
                   </li>
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Chemistry/index.html"
-                    >Chemistry</a
-                  >
+                    <a href="../../../Pages/Resources/Chemistry/index.html">Chemistry</a>
                   </li>
                   <li>
-                  <a
-                    href="../../../Pages/Resources/Music/index.html"
-                    >Music</a
-                  >
+                    <a href="../../../Pages/Resources/Music/index.html">Music</a>
                   </li>
                   <li>
-                    <a href="../../../Pages/Resources/Useful_Links/index.html"
-                      >Useful Links</a
-                    >
+                    <a href="../../../Pages/Resources/Useful_Links/index.html">Useful Links</a>
                   </li>
                 </ul>
               </div>
             </li>
             <li>
-              <a class="navbar-item" href="../../../index.html#contact"
-                >Contact</a
-              >
+              <a class="navbar-item" href="../../../index.html#contact">Contact</a>
             </li>
           </ul>
         </div>
